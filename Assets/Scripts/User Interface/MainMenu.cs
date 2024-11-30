c35ecaa455f150ea70d88fa19264decb91427ac0
Cideval <valleriandicque10@gmail.com>
2024-11-30 01:30:01 +0100

    Intégration des graphismes sur le main menu

--- Assets/Scripts/User Interface/MainMenu.cs
+++ Assets/Scripts/User Interface/MainMenu.cs
@@ -1,12 +1,43 @@
 ﻿using System.Collections;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Video;
+using DG.Tweening;
+using UnityEngine.UI;
 
 namespace Assets.Scripts.User_Interface
 {
     public class MainMenu : MonoBehaviour
     {
-        public void OnClick_Play()
+        [SerializeField, Tooltip("Fade in duration in seconds")] private float fadeInDuration = 1f;
+         
+        private VideoPlayer m_videoPlayer;
+
+        [ContextMenu("Skip transition")]
+        public void SkipTransition() => m_videoPlayer.time = m_videoPlayer.length;
+
+        private void Awake()
+        {
+            GetComponentInChildren<CanvasGroup>().alpha = 0;
+
+            m_videoPlayer = FindFirstObjectByType<VideoPlayer>();
+            m_videoPlayer.loopPointReached += OnTransitionEnd;
+        }
+
+        private void OnTransitionEnd(VideoPlayer videoPlayer)
+        {
+            GetComponentInChildren<CanvasGroup>().DOFade(1, fadeInDuration).OnComplete(EnableButtonsInteraction);
+        }
+
+        private void EnableButtonsInteraction()
+        {
+            foreach (Button button in GetComponentsInChildren<Button>())
+            {
+                button.interactable = true;
+            }
+        }
+
+        public void OnClick_Start()
         {
             MenuManager.ChangeMenuState(Menu.MAIN_MENU, MenuState.CLOSE);
 
